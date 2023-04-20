9dba27965689435d9a3f82b328a195d941b1ed5e
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-04-20 09:04:37 -0700

    with members page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 return (
 <div className="text-center py-5">
 <h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">Hello World</h1>
-<p className="mt-6 text-lg leading-8 text-gray-400">Isn't this grande</p>
+<p className="mt-6 text-lg leading-8 text-gray-400">Isn`t this grande</p>
 </div>
 )
 }
