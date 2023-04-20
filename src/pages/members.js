a386811072df2bd50532b18fd8dc333aa875d5ed
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-04-20 09:05:02 -0700

    with members page2

---
+++ src/pages/members.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+return (
+<div className="text-center py-5">
+<h1 className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">Members</h1>
+<p className="mt-6 text-lg leading-8 text-gray-400">Isn`t this grande</p>
+</div>
+)
+}
